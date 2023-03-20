1e3408601e4906c569d9582b5a51ca6a9395c7ff
dgvdn <duongvanduan2601@gmail.com>
2023-03-20 18:57:15 +0700

    create project

---
+++ src/main/java/com/example/waterbilling/WaterBillingDashboardSqaApplication.java
@@ -0,0 +1,13 @@
+package com.example.waterbilling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterBillingDashboardSqaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterBillingDashboardSqaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/waterbilling/WaterBillingDashboardSqaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.waterbilling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterBillingDashboardSqaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
